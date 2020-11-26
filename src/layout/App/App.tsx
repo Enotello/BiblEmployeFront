e76ba6afa8cf30d9ebbe6ae2d0151af2aac6fa69
Ekaterina <brekhova.katya@gmail.com>
2020-11-26 14:24:34 +0300

    finishing auth

--- src/layout/App/App.tsx
+++ src/layout/App/App.tsx
@@ -3,49 +3,66 @@ import './App.css'
 import "antd/dist/antd.css";
 import {Layout, Breadcrumb, Button, Space, Modal} from 'antd';
 import ListBook from "../../page/ListBook";
-import {Switch, Route, BrowserRouter, NavLink} from "react-router-dom";
+import {Switch, Route, BrowserRouter, NavLink, Redirect} from "react-router-dom";
 import IssueBook from "../../page/IssueBook";
 import Register from "../../page/Auth/Register/Register";
 import Login from "../../page/Auth/Login/Login";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../store/reducers";
+import {logout} from "../../store/actions/auth";
 
 const {Header, Content, Footer} = Layout;
 
 
 const App = () => {
+  const authState: any = useSelector<RootState>((store) => store.auth);
+  const dispatch = useDispatch();
+  const stableDispatch = useCallback(dispatch, []);
 
-    return (
+  return (
+    <BrowserRouter>
+      <Layout className="layout">
+        <Header className="header">
+          <div className='logo'>
+            Электронная библиотека
+          </div>
+          {(authState.token)
+            ? <>
+              <nav className='links'>
+                <ul>
+                  <li><NavLink to={'#'} style={{color: 'white'}} className='link'>Ссылка 1</NavLink></li>
+                  <li><NavLink to={'#'} style={{color: 'white'}} className='link'>Ссылка 2</NavLink></li>
+                  <li><NavLink to={'#'} style={{color: 'white'}} className='link'>Ссылка 3</NavLink></li>
+                </ul>
+              </nav>
+              <div className='exit'>
+                <Button type='text' onClick={() => stableDispatch(logout())}>Выход</Button>
+              </div>
+            </>
+            : null
+          }
+        </Header>
+        <Content style={{padding: '50px', paddingBottom: '10px'}}>
+          <div className="site-layout-content">
+            <Switch>
+              {(authState.token)
+                ? <>
+                  <Route path="/issue/:uid" component={IssueBook}/>
+                  <Route path="/register" component={Register}/>
+                  <Route path="/" component={ListBook}/>
+                </>
 
-        <BrowserRouter>
-            <Layout className="layout">
-                <Header className="header">
-                    <div className='logo'>
-                        Электронная библиотека
-                    </div>
-                    <nav className='links'>
-                        <ul>
-                            <li><NavLink to={'#'} style={{color: 'white'}} className='link'>Ссылка 1</NavLink></li>
-                            <li><NavLink to={'#'} style={{color: 'white'}} className='link'>Ссылка 2</NavLink></li>
-                            <li><NavLink to={'#'} style={{color: 'white'}} className='link'>Ссылка 3</NavLink></li>
-                        </ul>
-                    </nav>
-                    <div className='exit'>
-                        <Button type='link' onClick={() => {
-                        }}>Выход</Button>
-                    </div>
-                </Header>
-                <Content style={{padding: '50px', paddingBottom: '10px'}}>
-                    <div className="site-layout-content">
-                        <Switch>
-                            <Route path="/issue/:uid" component={IssueBook}/>
-                            <Route path="/register" component={Register}/>
-                            <Route path="/login" component={Login}/>
-                            <Route path="/" component={ListBook}/>
-                        </Switch>
-                    </div>
+                : <>
+                  <Redirect to={'/login'}/>
+                  <Route path="/login" component={Login}/>
+                </>}
 
-                </Content>
-            </Layout>
-        </BrowserRouter>)
+            </Switch>
+          </div>
+
+        </Content>
+      </Layout>
+    </BrowserRouter>)
 
 }
 
